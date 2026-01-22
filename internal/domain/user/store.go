68c3c9fa5f6143662e417ddf587b93e3ce2e35a4
chetanuchiha16 <chetan16ck@gmail.com>
2026-01-22 21:27:21 +0530

    store

--- internal/domain/user/store.go
+++ internal/domain/user/store.go
@@ -1,8 +1,26 @@
 package user
 
-import "sync"
+import (
+	"context"
 
-var (
-	CacheMutex sync.RWMutex
-	UserCache  = make(map[int]User)
+	"github.com/chetanuchiha16/go-play/db"
+	"github.com/jackc/pgx/v5/pgxpool"
 )
+
+type Store interface {
+	GetUser(ctx context.Context, id int64) (db.User, error)
+	CreateUser(ctx context.Context, arg db.CreateUserParams) (db.User, error)
+	DeleteUser(ctx context.Context, id int64) error
+}
+
+type SQLStore struct {
+	pool        *pgxpool.Pool
+	*db.Queries // get create del user are methods of *db.queries they are now belongs to Mystore ?
+}
+
+func NewStore(pool *pgxpool.Pool) *SQLStore {
+	return &SQLStore{
+		pool:    pool,
+		Queries: db.New(pool),
+	}
+}
