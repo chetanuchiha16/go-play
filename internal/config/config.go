aa904eba5282904252cbe1435ad314287e32ec7d
chetanuchiha16 <chetan16ck@gmail.com>
2026-01-22 16:41:31 +0530

    db configs

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,25 @@
 package config
+
+import (
+	"log"
+
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	DATABASE_URL string `mapstructure:"DATABASE_URL"`
+}
+
+func Load() Config {
+	viper.SetConfigFile(".env")
+	viper.AutomaticEnv()
+	if err := viper.ReadInConfig(); err != nil{
+		log.Println("no env file found",)
+	}
+
+	var cfg Config
+	if err := viper.Unmarshal(&cfg); err != nil {
+		log.Fatal("Unable to decode into the struct")
+	}
+	return cfg
+}
--- pkg/database/postgres.go
+++ pkg/database/postgres.go
@@ -1,1 +1,18 @@
 package database
+
+import (
+	"context"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+func NewPool(ctx context.Context, connStr string) (*pgxpool.Pool, error) {
+	// pool, err := pgxpool.New(ctx, connStr)
+	config, err := pgxpool.ParseConfig(connStr)
+	if err != nil {
+		return nil, err
+	}
+
+	return pgxpool.NewWithConfig(ctx, config)
+
+}
