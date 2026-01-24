9c18fb4970851ead2643b51cc3e2262d6902651a
chetanuchiha16 <chetan16ck@gmail.com>
2026-01-24 19:42:49 +0530

    user service skeleton

---
+++ internal/domain/user/service.go
@@ -0,0 +1,35 @@
+package user
+
+import (
+	"context"
+
+	"github.com/chetanuchiha16/go-play/db"
+)
+
+type Service interface {
+	CreateUser(ctx context.Context, args db.CreateUserParams) (db.User, error)
+	GetUser(ctx context.Context, id int64) (db.User, error)
+	DeleteUser(ctx context.Context, id int64) error
+}
+
+type userService struct {
+	store Store
+}
+
+func NewService(s Store) *userService {
+	return &userService{
+		store: s,
+	}
+}
+
+func (s *userService) CreateUser(ctx context.Context, args db.CreateUserParams) (db.User, error) {
+	return s.store.CreateUser(ctx, args)
+}
+
+func (s *userService) GetUser(ctx context.Context, id int64) (db.User, error) {
+	return s.store.GetUser(ctx, id)
+}
+
+func (s *userService) DeleteUser(ctx context.Context, id int64) error {
+	return s.store.DeleteUser(ctx, id)
+}
