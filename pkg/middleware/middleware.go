cbd70f7bf1938c61efe69c8701310219ea259d1a
chetanuchiha16 <chetan16ck@gmail.com>
2026-02-03 17:41:49 +0530

    understood middleware at some level

--- pkg/middleware/middleware.go
+++ pkg/middleware/middleware.go
@@ -32,17 +32,17 @@ type loggingResponseWriter struct {
 	statusCode int
 }
 
-func (lrw *loggingResponseWriter) WriteHeader(code int) {
+func (lrw *loggingResponseWriter) WriteHeader(code int) { // our handler calls this version
 	lrw.statusCode = code
 	lrw.ResponseWriter.WriteHeader(code)
 }
 
 func LoggerMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) { //HadleFunc is of type func
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) { //HadleFunc is of type func, this is a type conversion so it satisfy the interface and can call ServeHttp method
 		start := time.Now()
 		lrw := &loggingResponseWriter{w, http.StatusOK}
 
-		next.ServeHTTP(lrw, r)
+		next.ServeHTTP(lrw, r) // call next by providing lrw and r
 
 		// 1. Determine the color for the status code ONLY
 		var statusColor string
