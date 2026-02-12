f40e5d0cf4eb8f7a7ccd7211fcdef5090bd739d1
chetanuchiha16 <chetan16ck@gmail.com>
2026-02-12 21:28:39 +0530

    implemented ValidateToken function in users/helper.go

--- internal/domain/user/helper.go
+++ internal/domain/user/helper.go
@@ -2,7 +2,7 @@ package user
 
 import (
 	"time"
-
+	"fmt"
 	"github.com/chetanuchiha16/go-play/internal/config"
 	"github.com/golang-jwt/jwt/v4"
 )
@@ -18,3 +18,22 @@ func GenerateToken(user_id int64) (string, error) {
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256,claims)
 	return token.SignedString(jwtkey)
 }
+
+func ValidateToken(tokenString string) (jwt.MapClaims, error) {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method %v",token.Header["alg"])
+		}
+		return jwtkey, nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		return claims, nil
+	}
+
+	return nil, fmt.Errorf("Invalid token")
+} 
