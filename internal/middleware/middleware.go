5a66951ff4c03c5f524c322eb0004cf8af5b72ec
chetanuchiha16 <chetan16ck@gmail.com>
2026-02-23 22:09:12 +0530

    refactor RequestId middleware comments for clarity

--- internal/middleware/middleware.go
+++ internal/middleware/middleware.go
@@ -44,9 +44,9 @@ func RequestIdMiddleWare(next http.Handler) http.Handler {
 	return http.HandlerFunc(func (w http.ResponseWriter, r * http.Request) {
 
 		request_id := fmt.Sprintf("%d", time.Now().UnixNano())
-		ctx := context.WithValue(r.Context(), "request_id", request_id)
+		ctx := context.WithValue(r.Context(), "request_id", request_id) // take request context and store in our context
 		w.Header().Set("X-Request-ID", request_id)
-		next.ServeHTTP(w, r.WithContext(ctx))
+		next.ServeHTTP(w, r.WithContext(ctx)) // now take our updated context and put it back to requst context
 	})
 }
 
