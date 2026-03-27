3d0ad61f973a37f54c78e54b75d8604e7de08ef0
chetanuchiha16 <chetan16ck@gmail.com>
2026-03-27 17:18:59 +0530

    custom types request_id

--- internal/middleware/middleware-manager.go
+++ internal/middleware/middleware-manager.go
@@ -1,8 +1,8 @@
 package middleware
 
-import "github.com/golang-jwt/jwt/v4"
+// import "github.com/golang-jwt/jwt/v4"
 
-type tokenValidator func(tokenString string) (jwt.MapClaims, error)
+// type tokenValidator func(tokenString string) (jwt.MapClaims, error)
 type MiddlewareManager struct {
 	// jwtkey []byte
 	// validateToken tokenValidator
--- internal/middleware/requestId-middlware.go
+++ internal/middleware/requestId-middlware.go
@@ -5,13 +5,17 @@ import (
 	"fmt"
 	"net/http"
 	"time"
+
+	"github.com/chetanuchiha16/go-play/pkg"
 )
+
+const id pkg.ContextKey = "request_id"
 
 func (MiddlewareManager) RequestIdMiddleWare(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
 		request_id := fmt.Sprintf("%d", time.Now().UnixNano())
-		ctx := context.WithValue(r.Context(), "request_id", request_id) // take request context and store in our context
+		ctx := context.WithValue(r.Context(), id, request_id) // take request context and store in our context
 		w.Header().Set("X-Request-ID", request_id)
 		next.ServeHTTP(w, r.WithContext(ctx)) // now take our updated context and put it back to requst context
 	})
