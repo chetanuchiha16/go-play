3cae2b1b4751eda72052f0903a385797fbba659e
chetanuchiha16 <chetan16ck@gmail.com>
2026-04-04 14:25:07 +0530

    refactor response helpers to use consistent resource naming in response messages

--- pkg/response/helpers.go
+++ pkg/response/helpers.go
@@ -1,44 +1,49 @@
 package response
 
 import (
+	"fmt"
 	"net/http"
 )
 
-func Created[T any](data T, messages ...string) GenericResponse[T] {
-	message := "Resource created successfully"
-
-	if len(messages) > 0 {
-		message = messages[0]
+func getOverrides(override []string) string {
+	if len(override) > 0 {
+		return override[0]
 	}
+	return "Resource"
+}
+func Created[T any](data T, resourceName ...string) GenericResponse[T] {
+	name := getOverrides(resourceName)
+
+	// Use a template for consistency
+	message := fmt.Sprintf("%s created successfully", name)
 
 	return Success(http.StatusCreated, data, message)
 }
 
-func Detail[T any](data T, messages ...string) GenericResponse[T] {
-	message := "Resource retrieved successfully"
+func Detail[T any](data T, resourceName ...string) GenericResponse[T] {
+	// Default subject
+	name := getOverrides(resourceName)
+	message := fmt.Sprintf("%s retrieved successfully", name)
+	return Success(http.StatusOK, data, message)
+}
 
-	if len(messages) > 0 {
-		message = messages[0]
+func List[T any](data []T, resourceName ...string) GenericResponse[[]T] {
+	// Default subject
+	subject := getOverrides(resourceName)
+	if len(resourceName) == 0 {
+		subject = "Resources"
 	}
+	// Use a template for consistency
+	message := fmt.Sprintf("%s retrieved successfully", subject)
 
 	return Success(http.StatusOK, data, message)
 }
 
-func List[T any](data []T, messages ...string) GenericResponse[[]T] {
-	message := "Resources retrieved successfully"
+func Deleted(resourceName ...string) GenericResponse[struct{}] {
+	// Default subject
+	subject := getOverrides(resourceName)
 
-	if len(messages) > 0 {
-		message = messages[0]
-	}
-
-	return Success(http.StatusOK, data, message)
-}
-
-func Deleted(messages ...string) GenericResponse[struct{}] {
-	message := "Resource deleted successfully"
-
-	if len(messages) > 0 {
-		message = messages[0]
-	}
+	// Use a template for consistency
+	message := fmt.Sprintf("%s deleted successfully", subject)
 	return Success(http.StatusOK, struct{}{}, message)
 }
