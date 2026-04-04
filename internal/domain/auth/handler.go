32d8c39cba4fd6303b4f96e887cfbd04e0c13161
chetanuchiha16 <chetan16ck@gmail.com>
2026-04-04 14:38:16 +0530

    generic response in Login

--- internal/domain/auth/handler.go
+++ internal/domain/auth/handler.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/chetanuchiha16/go-play/internal/domain/user"
 	"github.com/chetanuchiha16/go-play/internal/errors"
+	"github.com/chetanuchiha16/go-play/pkg/response"
 	"github.com/go-fuego/fuego"
 	"github.com/go-fuego/fuego/option"
 )
@@ -25,19 +26,21 @@ func (h *Handler) RegisterAuthRoutes(s *fuego.Server, authmw func(http.Handler) 
 }
 
 // Use the LoginResponse struct instead of map[string]string
-func (h *Handler) Login(c fuego.ContextWithBody[LoginRequest]) (LoginResponse, error) {
+func (h *Handler) Login(c fuego.ContextWithBody[LoginRequest]) (response.GenericResponse[LoginResponse], error) {
 	body, err := c.Body()
 	if err != nil {
-		return LoginResponse{}, err
+		return response.GenericResponse[LoginResponse]{}, err
 	}
 	_user, token, err := h.service.Login(c.Context(), body.Email, body.Password)
 	if err != nil {
-		return LoginResponse{}, errors.MapError(err, "user")
+		return response.GenericResponse[LoginResponse]{}, errors.MapError(err, "user")
 	}
 
 	// Return a structured response that Swagger can read
-	return LoginResponse{
-		Token: token,
-		User: user.NewUserResponse(_user),
+	return response.GenericResponse[LoginResponse]{
+		Data: &LoginResponse{
+			Token: token,
+			User:  user.NewUserResponse(_user),
+		},
 	}, nil
 }
