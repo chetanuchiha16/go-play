b4e16535e3098d65d243181d1af32b88ee512025
chetanuchiha16 <chetan16ck@gmail.com>
2026-04-04 13:36:21 +0530

    added generic get and list response

--- pkg/response/helpers.go
+++ pkg/response/helpers.go
@@ -12,6 +12,27 @@ func Created[T any](data T, messages ...string) {
 	Success(http.StatusCreated, data, message)
 }
 
+func Detail[T any](data T, messages ...string) {
+    message := "Resource retrieved successfully"
+
+    if len(messages) > 0 {
+        message = messages[0]
+    }
+
+    Success(http.StatusOK, data, message)
+}
+
+func List[T any](data []T, messages ...string) {
+    message := "Resources retrieved successfully"
+
+    if len(messages) > 0 {
+        message = messages[0]
+    }
+
+    Success(http.StatusOK, data, message)
+}
+
 func Deleted() {
 	Success(http.StatusOK, struct{}{}, "Resource deleted successfully")
 }
+
