f7704440bd0f63cae6a618b2904da2c35552fde0
chetanuchiha16 <chetan16ck@gmail.com>
2026-05-17 09:46:04 +0530

    chore: add IF NOT EXISTS clauses to all database table creation and drop statements

--- db/migrations/20260511133042_rbac_tables.sql
+++ db/migrations/20260511133042_rbac_tables.sql
@@ -1,23 +1,23 @@
 -- +goose Up
-CREATE TABLE permissions (
+CREATE TABLE IF NOT EXISTS permissions (
     id SERIAL PRIMARY KEY,
     name VARCHAR(50) UNIQUE NOT NULL
 );
 
-CREATE TABLE roles (
+CREATE TABLE IF NOT EXISTS roles (
     id SERIAL PRIMARY KEY,
     name VARCHAR(50) UNIQUE NOT NULL
 );
 
 -- Pivot table for users and roles
-CREATE TABLE user_role (
+CREATE TABLE IF NOT EXISTS user_role (
     user_id INT REFERENCES users(id),
     perm_id INT REFERENCES permissions(id),
     PRIMARY KEY (user_id, perm_id)
 );
 
 -- Pivot table for roles and permissions
-CREATE TABLE role_permissions (
+CREATE TABLE IF NOT EXISTS role_permissions (
     role_id INT REFERENCES roles(id),
     perm_id INT REFERENCES permissions(id),
     PRIMARY KEY (role_id, perm_id)
